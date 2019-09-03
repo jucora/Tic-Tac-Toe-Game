e268b50c46562be82a41f07e638a63e40baa2f4c
Julian <artjulius@hotmail.com>
2019-09-03 17:50:39 -0500

    Global variables and Player class removed

--- bin/main.rb
+++ bin/main.rb
@@ -5,25 +5,29 @@ module UserInteraction
         puts "Which player do you prefer?"
         puts "1 for X or 2 for O"
         puts "==========================="
-        $rol = gets.chomp.to_i
+        @rol = gets.chomp.to_i
            
     end
+
     def input_cell
         @cell = nil
         puts "\nSelect a number in the cell"
         @cell = gets.chomp.to_i
     end
+
     def again?
         puts "##################"
-        puts "#PLAY AGAIN? (y/n)"
+        puts "\n#PLAY AGAIN? (y/n)"
         puts "##################"
-        $again = gets.chomp
-        if $again == "y" || $again == "Y"
-            player = Player.new
-            player.validate_rol
-        elsif $again == "n" || $again == "N" 
-            puts "Adios Amigo!" 
+        @again = gets.chomp
+        if @again === "y" || @again === "Y"
+            validate_rol
+        elsif @again === "n" || @again === "N"
+            puts "Adios Amigo!"
             exit
+        else
+            puts "Invalid option, please try again! "
+            again?
         end
     end
 end
